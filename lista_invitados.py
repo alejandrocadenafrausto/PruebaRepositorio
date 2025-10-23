a8a7b9263a62ed33d225edbe7255b97d35f6ebc6
alejandrocadenafrausto <alejandrocadenafrausto@gmail.com>
2025-10-23 10:38:22 -0600

    Add files via upload

---
+++ lista_invitados.py
@@ -0,0 +1,15 @@
+
+
+invitados = ['Andrea',"Max","yoyo","Nadia","Ximena"]
+mensaje = "te invito a mi fiesta"
+
+print(f"{invitados[0]} {mensaje}")
+print(f"{invitados[1]} {mensaje}")
+print(f"{invitados[2]} {mensaje}")
+print(f"{invitados[3]} {mensaje}")
+print(f"{invitados[4]} {mensaje}")
+
+
+mensaje_no_confirmacion = "no podrá asistir"
+print(f"{invitados[2]} {mensaje_no_confirmacion}")
+
